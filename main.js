b50bf10721dc8375d481a00b5a29273259773700
chen.zhang <zhangchen64d@gmail.com>
2023-03-11 23:24:33 +0900

    update

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 import './style.css';
+
+const leftHandle = document.querySelector('.left-handle');
+
+const rightHandle = document.querySelector('.right-handle');
+
+leftHandle.addEventListener('click', () => {
+  onHandleClick('left');
+});
+
+rightHandle.addEventListener('click', () => {
+  onHandleClick('right');
+});
+
+function onHandleClick(direction) {
+  // console.log('direction', direction);
+
+  const slider = document.querySelector('.slider');
+  // console.log('slider', slider);
+
+  const sliderIdx = getComputedStyle(slider).getPropertyValue('--slider-index');
+  // console.log('sliderIdx', sliderIdx);
+
+  if (direction === 'left') {
+    slider.style.setProperty('--slider-index', Number(sliderIdx) - 1);
+  }
+
+  if (direction === 'right') {
+    slider.style.setProperty('--slider-index', Number(sliderIdx) + 1);
+  }
+}
