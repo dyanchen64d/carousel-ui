3af29dd6702d6d064dddfc9bd40067993a69d245
chen.zhang <zhangchen64d@gmail.com>
2023-03-12 00:46:21 +0900

    update

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const leftHandle = document.querySelector('.left-handle');
 
 const rightHandle = document.querySelector('.right-handle');
 
+const slider = document.querySelector('.slider');
+
+const progressBar = document.querySelector('.progress-bar');
+
 leftHandle.addEventListener('click', () => {
   onHandleClick('left');
 });
@@ -13,11 +17,6 @@ rightHandle.addEventListener('click', () => {
 });
 
 function onHandleClick(direction) {
-  // console.log('direction', direction);
-
-  const slider = document.querySelector('.slider');
-  // console.log('slider', slider);
-
   const sliderIdx = getComputedStyle(slider).getPropertyValue('--slider-index');
   // console.log('sliderIdx', sliderIdx);
 
@@ -28,4 +27,26 @@ function onHandleClick(direction) {
   if (direction === 'right') {
     slider.style.setProperty('--slider-index', Number(sliderIdx) + 1);
   }
+
+  calcProgressBar(progressBar);
 }
+
+function calcProgressBar(pb) {
+  pb.innerHTML = '';
+  const itemCount = slider.children.length;
+
+  const itemPerScreen =
+    getComputedStyle(slider).getPropertyValue('--items-per-screen');
+
+  const progressBarItemCount = Math.ceil(itemCount / itemPerScreen);
+
+  const sliderIdx = getComputedStyle(slider).getPropertyValue('--slider-index');
+
+  for (let i = 0; i < progressBarItemCount; i++) {
+    pb.innerHTML += `
+      <div class="progress-item ${i === Number(sliderIdx) ? 'active' : ''}" />
+    `;
+  }
+}
+
+calcProgressBar(progressBar);
